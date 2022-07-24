747d45bd4b841d4b8d39116e4fc14ad2160072c7
Matheus José <matheus.reis@asaconsultoria.com.br>
2022-07-24 16:25:02 -0300

    [UPDATE][IN PROGRESS] Criando efeitos da imagem do produto.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+window.onload = () => { 
+  setImagemProduto("https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80");
+};
+
 function adicionarComentario() {
     openModal();
 }
@@ -6,12 +10,14 @@ function adicionarComentario() {
 function openModal() {
     // document.getElementById("backdrop").style.display = "block"
     document.getElementById("idModalComentario").style.display = "block"
-    document.getElementById("idModalComentario").classList.add("show")
+    document.getElementById("idModalComentario").classList.add("show");
 }
+
+
 function closeModal() {
     // document.getElementById("backdrop").style.display = "none"
-    document.getElementById("idModalComentario").style.display = "none"
-    document.getElementById("idModalComentario").classList.remove("show")
+    document.getElementById("idModalComentario").style.display = "none";
+    document.getElementById("idModalComentario").classList.remove("show");
 }
 
 function salvarComentario() {
@@ -25,7 +31,12 @@ function salvarComentario() {
         if (result.isConfirmed) {
           Swal.fire('Comentário salvo com sucesso!', '', 'success')
         }
-      })
-      
+      }).then(() => {
+        closeModal();
+      });
 }
 
+function setImagemProduto(url) { 
+  console.log(document.getElementById("imgProdutoAnuncio"));
+  document.getElementById("imgProdutoAnuncio").src = url;
+}
